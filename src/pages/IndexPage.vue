682591fc856323c04021336ca309a73b1a7d6982
pikaCode144 <1447881964@qq.com>
2023-09-06 18:07:35 +0800

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page> -->
+  <div class="q-pa-none q-gutter-sm">
+    <q-btn
+      color="white"
+      text-color="black"
+      label="Standard"
+      @click="openDialog"
+    />
+    <q-btn color="primary" label="Primary" />
+    <q-btn color="secondary" label="Secondary" />
+    <q-btn color="amber" glossy label="Amber" />
+    <q-btn color="brown-5" label="Brown 5" />
+    <q-btn color="deep-orange" glossy label="Deep Orange" />
+    <q-btn color="purple" label="Purple" />
+    <q-btn color="black" label="Black" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  methods: {
+    openDialog() {
+      this.$q
+        .dialog({
+          title: "Alert",
+          message: "Some message",
+        })
+        .onOk(() => {
+          // console.log('OK')
+        })
+        .onCancel(() => {
+          // console.log('Cancel')
+        })
+        .onDismiss(() => {
+          // console.log('I am triggered on both OK and Cancel')
+        });
+    },
+  },
+});
+</script>
